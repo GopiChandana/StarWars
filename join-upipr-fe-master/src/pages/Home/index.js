fa745d1cf3d96a7d2ca7865b441f4c7db3914126
gopichandana <gopichandanasiri@gmail.com>
2021-05-30 01:42:26 +0530

    Search Bar implementation done

--- join-upipr-fe-master/src/pages/Home/index.js
+++ join-upipr-fe-master/src/pages/Home/index.js
@@ -1,35 +1,118 @@
 import React, { useState } from 'react';
+import { useHistory } from 'react-router';
 import axios from "axios";
 import logo from './star-wars-logo.png';
 import './index.css';
 
 function HomePage() {
   const [query,setQuery]=useState("")
+  const [results,setResults]=useState([])
+  const [activequeryIndex,setActivequeryIndex]=useState(-1)
+  const inputref=React.useRef()
+  const scrollref=React.useRef()
+  const history=useHistory()
   const erasequery=()=>{
+  inputref.current.value="";
   setQuery("")
+  setResults([])
+  setActivequeryIndex(-1)
   }
-  const handlequery=(e)=>{
-    setQuery(e.target.value)
-    axios.get(`https://swapi.dev/api/people/?search=${query}`)
-    .then(res=>console.log(res))
-    
+  React.useEffect(()=>{
+    if(query.length>0)
+    {
+   axios.get(`https://swapi.dev/api/people/?search=${query}`)
+    .then(res=>setResults(res.data.results))
+    }
+   if(activequeryIndex>-1 && results.length>0)
+   {
+     inputref.current.value=results[activequeryIndex].name
+   }
+  },[query,activequeryIndex])
+  const handleSearchquery=()=>{
+    let urlsplitter=results && activequeryIndex>-1 && results[activequeryIndex].url.split("/")
+    let id=urlsplitter[urlsplitter.length-2]
+    history.push(`person/${id}`)
   }
- 
+  let delay=500;
+  const handlequery=debouncer((e)=>{
+    setQuery(e)
+  
+  },delay=500)
+  const handleKeyUp = (e)=>{
+    switch(e.keyCode)
+    {
+      case 13:
+       handleSearchquery();
+       break;
+       case 38:
+       scrollref.current.scrollTop-=20;
+       if(activequeryIndex<0)
+       {
+         setActivequeryIndex((results && results.length-1))
+       }
+       setActivequeryIndex((prev)=>prev-1);
+       if(activequeryIndex===-1)
+       {
+         setActivequeryIndex((results && results.length-1))
+       }
+
+       break;
+       case 40:
+        scrollref.current.scrollTop+=20;
+        if(activequeryIndex===results.length-1)
+        {
+          setActivequeryIndex(-1)
+        }
+        setActivequeryIndex((prev)=>prev+1);
+        if(activequeryIndex===results.length-1)
+        {
+          setActivequeryIndex(0)
+        }
+        break;
+      default:
+      break;
+    }
+  }
+  function debouncer(fn,delay=500){
+    let timer;
+    return function(){
+       clearTimeout(timer);
+       timer=setTimeout(()=>{
+         fn.apply(this,arguments)
+       },delay)
+    }
+  }
+  console.log(results)
   return (
-    <div>
+    <div onKeyUp={handleKeyUp} >
       <div className="logo">
         <img src={logo} alt="Star Wars Logo" />
       </div>
-      <div className="search-input">
+      <div className={results.length===0 || query===""?"search-input" : "search-input search-results"}>
       <div className="x_div">
-      <input  placeholder="Search by name" value={query} onChange={handlequery}/>
-      {query.length>0 && <div onClick={erasequery}>X</div>}
+      <input  placeholder="Search by name" ref={inputref} onChange={(e)=>handlequery(e.target.value)}/>
+      {query!=="" && <div onClick={erasequery}>X</div>}
       </div>
+      {results.length>0 && <div className="boundary1"></div>}
       <div className="search_icon">
       <i class="fa fa-search"></i>
       </div>
       </div>
-      
+     
+      {(results.length>0 && query!=="" ? <div ref={scrollref} className={results.length>=4 ? "results_box" :"results_box height_optimization"}>
+      {results.map((item,i)=>(
+       
+          <div className={i!==activequeryIndex?"results_innerbox":"results_innerbox results_innerbox1"}>
+          <div className="results_inner_firstbox">
+          <div>{item.name}</div>
+        <div className="birth_year">{item.birth_year}</div>
+        </div>
+        <div className="results_inner_secondbox">{item.gender}</div>
+        </div>
+       
+      ) 
+      )}
+      </div> : (query.length>0) ? <p>Enter a Valid Character Name</p> : null )}
     </div>
   );
 }
--- join-upipr-fe-master/src/pages/Person/index.js
+++ join-upipr-fe-master/src/pages/Person/index.js
@@ -1,7 +1,15 @@
 import React from 'react';
+import { useLocation } from 'react-router-dom';
 import './index.css';
 
 function Person() {
+  React.useEffect(()=>{
+    
+  },[])
+  const location = useLocation();
+  console.log(location.pathname)
+  let urlsplitter=location.pathname.split("/")
+  
   return (
     <div className="person">
       <h1>Luke Skywalker</h1>
