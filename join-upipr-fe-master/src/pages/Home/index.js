134a0a5f22fd37aa17461e8e7771980b65423769
gopichandana <gopichandanasiri@gmail.com>
2021-05-31 01:05:37 +0530

    Made some changes in improvising code

--- join-upipr-fe-master/src/pages/Home/index.js
+++ join-upipr-fe-master/src/pages/Home/index.js
@@ -8,10 +8,11 @@ function HomePage() {
   const [query, setQuery] = useState("");
   const [results, setResults] = useState([]);
   const [activequeryIndex, setActivequeryIndex] = useState(-1);
+  const [error, setError] = useState(false);
+  const [backspaceOn, setBackspaceOn] = useState(false);
   const inputref = React.useRef();
   const scrollref = React.useRef();
   const history = useHistory();
-
 
   const erasequery = () => {
     inputref.current.value = "";
@@ -20,34 +21,50 @@ function HomePage() {
     setActivequeryIndex(-1);
   };
 
-
   React.useEffect(() => {
-    if (query.length > 0) {
+    if (query.length > 0 && !backspaceOn) {
+      setError(false);
+      setBackspaceOn(false);
       axios
         .get(`https://swapi.dev/api/people/?search=${query}`)
         .then((res) => setResults(res.data.results));
     }
+
+    setBackspaceOn(false);
+  }, [query]);
+
+  React.useEffect(() => {
     if (activequeryIndex > -1 && results.length > 0) {
       inputref.current.value = results[activequeryIndex].name;
     }
-  }, [query, activequeryIndex]);
+  }, [activequeryIndex]);
 
-
-  const handleSearchquery = () => {
+  const handleSearchquery = (i) => {
+    if (query === "") {
+      setError(true);
+    } else {
+      setError(false);
+    }
+    setBackspaceOn(false);
     let urlsplitter =
       results &&
       activequeryIndex > -1 &&
       results[activequeryIndex].url.split("/");
     let id = urlsplitter[urlsplitter.length - 2];
-    history.push(`person/${id}`);
-  };
 
+    if (query !== "") {
+      if (id === undefined) {
+        history.push(`/error`);
+      } else {
+        history.push(`/person/${id}`);
+      }
+    }
+  };
 
   let delay = 500;
   const handlequery = debouncer((e) => {
     setQuery(e);
   }, (delay = 500));
-
 
   const handleKeyUp = (e) => {
     switch (e.keyCode) {
@@ -55,30 +72,33 @@ function HomePage() {
         handleSearchquery();
         break;
       case 38:
-        scrollref.current.scrollTop -= 20;
-        if (activequeryIndex < 0) {
-          setActivequeryIndex(results && results.length - 1);
+        scrollref.current.scrollTop -= 70;
+        if (activequeryIndex <= 0) {
+          scrollref.current.scrollTop = 580;
+          setActivequeryIndex(results && results.length);
         }
         setActivequeryIndex((prev) => prev - 1);
-        if (activequeryIndex === -1) {
-          setActivequeryIndex(results && results.length - 1);
-        }
         break;
       case 40:
-        scrollref.current.scrollTop += 20;
+        scrollref.current.scrollTop += 70;
         if (activequeryIndex === results.length - 1) {
+          scrollref.current.scrollTop = 0;
           setActivequeryIndex(-1);
         }
         setActivequeryIndex((prev) => prev + 1);
-        if (activequeryIndex === results.length - 1) {
-          setActivequeryIndex(0);
-        }
+        break;
+      case 8:
+        setBackspaceOn(true);
         break;
       default:
         break;
     }
   };
 
+  const handleClick = (elemdata) => {
+    let id = +elemdata.url.split("/")[5];
+    history.push(`/person/${id}`);
+  };
 
   function debouncer(fn, delay = 500) {
     let timer;
@@ -104,20 +124,25 @@ function HomePage() {
       >
         <div className="x_div">
           <input
+            type="text"
             placeholder="Search by name"
             ref={inputref}
+            required
             onChange={(e) => handlequery(e.target.value)}
           />
           {query !== "" && <div onClick={erasequery}>X</div>}
         </div>
         {results.length > 0 && <div className="boundary1"></div>}
+
         <div className="search_icon" onClick={handleSearchquery}>
           {query !== "" && results.length === 0 ? (
             <div className="spinner">
               <i class="fa fa-spinner"></i>
             </div>
           ) : (
-            <i class="fa fa-search"></i>
+            <div className="search">
+              <i class="fa fa-search"></i>
+            </div>
           )}
         </div>
       </div>
@@ -133,6 +158,7 @@ function HomePage() {
         >
           {results.map((item, i) => (
             <div
+              onClick={() => handleClick(item)}
               className={
                 i !== activequeryIndex
                   ? "results_innerbox"
@@ -150,6 +176,7 @@ function HomePage() {
       ) : query.length > 0 ? (
         <p>Enter a Valid Character Name</p>
       ) : null}
+      {error && <p>Input field should not be empty</p>}
     </div>
   );
 }
--- join-upipr-fe-master/src/pages/Person/index.js
+++ join-upipr-fe-master/src/pages/Person/index.js
@@ -4,41 +4,63 @@ import "./index.css";
 import axios from "axios";
 function Person() {
   const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(false);
   const location = useLocation();
   let urlsplitter = location.pathname.split("/");
   let id = urlsplitter[urlsplitter.length - 1];
 
   React.useEffect(() => {
+    setLoading(true);
     axios
       .get(`https://swapi.dev/api/people/${id}/`)
-      .then((res) => setData(res.data));
+
+      .then((res) => setData(res.data))
+      .catch((err) => console.log(err))
+      .finally(() => setLoading(false));
   }, []);
- 
-  
-  return (
+
+  return !loading ? (
     <div className="person">
       <div className="details_card">
         <p>
-          <div>Name:</div>
+          <div>Name </div>
+          <div>-</div>
           <strong>{data.name}</strong>
         </p>
         <p>
-          <div>BirthYear:</div>
+          <div>BirthYear </div>
+          <div>-</div>
           <strong>{data.birth_year}</strong>
         </p>
         <p>
-          <div>Gender:</div>
+          <div>Gender </div>
+          <div>-</div>
           <strong>{data.gender}</strong>
         </p>
         <p>
-          <div>EyeColour:</div>
+          <div>EyeColour</div>
+          <div>-</div>
           <strong>{data.eye_color}</strong>
         </p>
         <p>
-          <div>Height:</div>
+          <div>Height </div>
+          <div>-</div>
           <strong>{data.height}</strong>
         </p>
       </div>
+    </div>
+  ) : (
+    <div class="preloader">
+      <div className="loading">Loading</div>
+      <span class="line line-1"></span>
+      <span class="line line-2"></span>
+      <span class="line line-3"></span>
+      <span class="line line-4"></span>
+      <span class="line line-5"></span>
+      <span class="line line-6"></span>
+      <span class="line line-7"></span>
+      <span class="line line-8"></span>
+      <span class="line line-9"></span>
     </div>
   );
 }
